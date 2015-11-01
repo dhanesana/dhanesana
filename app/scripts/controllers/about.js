3c39c633a59675f5ac4de088034a3108cdefb12f
David P. Hanesana <dhanesana@gmail.com>
2015-11-01 02:45:51 -0800

    Generate AngularJS app via Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dhanesanaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dhanesanaApp
+ */
+angular.module('dhanesanaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dhanesanaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dhanesanaApp
+ */
+angular.module('dhanesanaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
